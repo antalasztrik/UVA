47c45f1c95481e028ce3ced67d55d646c8c6a267
antalasztrik <49885361+antalasztrik@users.noreply.github.com>
2019-05-25 22:37:31 +0200

    Update main.c

--- 11309/main.c
+++ 11309/main.c
@@ -6,53 +6,45 @@ int palindrome[2359] = {0};
 
 int solve(int x)
 {
-    x++;
-    while(palindrome[x % 2360] != 1)
-        x++;
-
-    return x % 2360;
+    while( palindrome[++x] != 1 && x < 2360 );
+    return x == 2360 ? 0 : x;
 }
 
-int palind(int x)
+
+int is_palindrome(int x)
 {
-    int b = 0, e;
     char s[5];
     sprintf(s, "%d", x);
-    e = strlen(s) - 1;
 
-    if(x % 100 >= 60)
-        return 0;
+    int b = 0, e = strlen(s) - 1;
 
     while(e > b)
-    {
         if(s[b++] != s[e--])
             return 0;
-    }
+
     return 1;
 }
 
 void gen()
 {
-    int i;
-    for(i = 0; i < 2359; i++)
-        if(palind(i))
-            palindrome[i] = 1;
+    int i, j;
+    for(i = 0; i < 24; i++)
+        for(j = 0; j < 60; j++)
+            if(is_palindrome((100 * i) + j))
+                palindrome[(100 * i) + j] = 1;
 }
 
 int main()
 {
-    int n, x, z;
-    char s[6];
-    scanf("%d", &n);
     gen();
-    while(n)
+    int N, result;
+    char s[6];
+    scanf("%d", &N);
+    while(N--)
     {
         scanf("%s", s);
-        x = 1000 * (s[0] - 48) + 100 * (s[1] - 48) + 10 * (s[3] - 48) + (s[4] - 48);
-        z = solve(x);
-        printf("%02d:%02d\n", z / 100, z % 100);
-        n--;
+        result = solve(100 * atoi(strtok(s, ":")) + atoi(strtok(NULL, "")));
+        printf("%02d:%02d\n", result / 100, result % 100);
     }
     return 0;
 }
-
