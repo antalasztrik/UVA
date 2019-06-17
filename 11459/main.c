5eab05e7852bed51f3488ad1d4907dc6a62b5473
antalasztrik <49885361+antalasztrik@users.noreply.github.com>
2019-06-17 02:51:47 +0200

    Update main.c

--- 11459/main.c
+++ 11459/main.c
@@ -2,43 +2,54 @@
 #include <stdlib.h>
 
 int players[1000000];
+int step[101];
+int a, b, c, winner;
+
+void init(){
+    winner = 0;
+    int i, x, p;
+    for(i = 0; i < a; i++)
+        players[i] = 1;
+
+    for(i = 0; i < 101; i++)
+        step[i] = i;
+
+
+    for(i = 0; i < b; i++){
+        scanf("%d %d", &x, &p);
+        step[x] = p;
+    }
+
+}
+
+void rolls(){
+    int i, x, j;
+    for(i = 0; i < c; i++)
+    {
+        scanf("%d", &x);
+        if(!winner)
+        {
+            j = players[i%a] + x;
+            if(step[j] < 100)
+                players[i%a] = step[j];
+            else
+            {
+                players[i%a] = 100;
+                winner = 1;
+            }
+        }
+    }
+}
 
 int main()
 {
-    int N, a, b, c, i, j, winner, x, p;
+    int N, i;
     scanf("%d", &N);
     while(N--)
     {
-        winner = 0;
         scanf("%d %d %d", &a, &b, &c);
-        int step[101];
-
-        for(i = 0; i < a; i++)
-            players[i] = 1;
-
-        for(i = 0; i < 101; i++)
-            step[i] = i;
-
-        for(i = 0; i < b; i++){
-            scanf("%d %d", &x, &p);
-            step[x] = p;
-        }
-
-        for(i = 0; i < c; i++)
-        {
-            scanf("%d", &x);
-            if(!winner)
-            {
-                j = players[i%a] + x;
-                if(step[j] < 100)
-                    players[i%a] = step[j];
-                else
-                {
-                    players[i%a] = 100;
-                    winner = 1;
-                }
-            }
-        }
+        init();
+        rolls();
 
         for(i = 0; i < a; i++)
             printf("Position of player %d is %d.\n", i + 1, players[i]);
